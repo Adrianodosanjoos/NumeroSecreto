b51fa604d91c2cd2934fab9b06dc3b0871257afd
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-05-19 15:39:54 -0300

    Atualização

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
 function exibirTextoNaTela(tag, texto){
 
 let campo = document.querySelector(tag);
 campo.innerHTML = texto;  
 }
 
-exibirTextoNaTela('h1',  'Jogo do Numero Secreto')
-exibirTextoNaTela('p', 'Escolha o número entre 1 e 10')
+exibirTextoNaTela('h1',  'Jogo do Número Secreto');
+exibirTextoNaTela('p', 'Escolha o Número entre 1 e 10');
 
 function verificarChute() {
-    console.log('O botão foi clicado')
+    let chute = document.querySelector('input').value;
+   if (chute == numeroSecreto) {
+    exibirTextoNaTela('h1', 'Acertou!');
+    let mensagemTentativas =`Você descobriu o Número Secreto com $!`;
+     exibirTextoNaTela('p', 'Você descobriu o Número Secreto!');
+   } else {
+    if (chute > numeroSecreto) {
+        exibirTextoNaTela('p', 'O Número  secreto é Menor ⬇!');
+    } else {
+        exibirTextoNaTela('p', 'O Número secreto é Maior ⬆!');
+    }
+   }
+}
+
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * 10 + 1);
 }
